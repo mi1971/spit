d639e585f40fd4ace11f77c6ef112dbcd74cd334
Matt Ireland <matt.ireland@mortgagechoice.com.au>
2014-08-30 12:54:45 +1000

    Added commitments

--- client/views/scenario/scenario.js
+++ client/views/scenario/scenario.js
@@ -45,3 +45,46 @@ Template.creditCards.events({
 Template.creditCards.hasCreditCards = function(){
     return Liabilities.find().count() > 0;
 }
+
+
+// Commitments
+
+Template.commitments.events({
+    'click #addCommitment': function(e) {
+        e.preventDefault();
+//        debugger;
+//        return;
+        var commitmentCount = Liabilities.find({type:"Commitment"}).count();
+        Liabilities.insert({
+            description: null, //"Credit Card " + (creditCardCount + 1).toString(),
+            limit: null,
+            owing: null,
+            type: "Commitment",
+            scenarioId: this.scenario._id
+        });
+        //$(".limit:last").focus().select();
+
+    },
+
+    'click .delete': function(e) {
+        e.preventDefault();
+        Liabilities.remove(this._id);
+
+    },
+    'change input': function(e,t){
+        var $tr = $(e.target).parents("tr")
+        var description = $tr.find(".description").val();
+        var payment = $tr.find(".payment").val();
+        var owing = $tr.find(".owing").val();
+        //alert(description + "\n"+ limit + "\n" + owing);
+        Liabilities.update({_id:this._id},{$set: {
+            description: description,
+            payment: payment,
+            owing: owing
+        }});
+    }
+});
+
+Template.commitments.hasCommitments = function(){
+    return Liabilities.find().count() > 0;
+}
